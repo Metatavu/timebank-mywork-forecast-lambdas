2f0a1c50fb62df3b2d3ea10d0d4f7f6d9cef022d
nojuord <104766185+nojuord@users.noreply.github.com>
2022-11-04 13:27:29 +0200

    Implemented listTimeEntries function

--- src/functions/list-time-entries/handler.ts
+++ src/functions/list-time-entries/handler.ts
@@ -1,9 +1,35 @@
 import { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 // import { parseBearerAuth } from '@libs/auth-utils';
 import { middyfy } from '@libs/lambda';
+import { time } from 'console';
+import { CreateForecastApiService, ForecastApiService } from 'src/apis/forecast-api-service';
 
-async function listTimeEntriesFunction(): Promise<any> {
+interface Parameters {
+  projectId: number
+}
 
+interface Response {
+  id: number,
+  person: number,
+  project: number,
+  task: number,
+  timeRegistered: number;
+}
+
+async function listTimeEntriesFunction(api: ForecastApiService, parameters: Parameters): Promise<Response[]> {
+  const timeEntries = await api.getTimeEntries(parameters.projectId);
+
+  const responseTimeEntries = timeEntries.map(timeEntry => {
+    return {
+      id: timeEntry.id,
+      person: timeEntry.person,
+      project: timeEntry.project,
+      task: timeEntry.task,
+      timeRegistered: timeEntry.time_registered,
+    }
+  })
+
+  return responseTimeEntries;
 }
 
 /**
@@ -25,7 +51,11 @@ const listTimeEntries: ValidatedEventAPIGatewayProxyEvent<any> = async event => 
 
   // TODO implement functoin logic
 
-  const timeEntries = await listTimeEntriesFunction();
+  const api = CreateForecastApiService();
+  
+  const timeEntries = await listTimeEntriesFunction(api, {
+    projectId: +event.queryStringParameters.projectId
+  });
   
   return {
     statusCode: 200,
