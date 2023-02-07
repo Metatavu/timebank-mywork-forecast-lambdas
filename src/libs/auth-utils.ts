9ee21cf2e94a36ec8093f1e2c9daaeb6738c216b
Jdallos <joshdallos@gmail.com>
2023-02-07 13:46:09 +0200

    Version changes

--- src/libs/auth-utils.ts
+++ src/libs/auth-utils.ts
@@ -1,10 +1,13 @@
 import moment from "moment";
 import { AccessToken } from "src/types";
 
+// TODO: this must be implemented before deployment
+// Use keycloak js
+// All moment uses should be using luxon
 /**
  * Returns true if token is valid, false otherwise
- * 
- * @param token Access token 
+ *
+ * @param token Access token
  */
 export const isTokenValid = (token: AccessToken) => {
   if (!token) {
