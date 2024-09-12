87cf4041323d1ddd3dd53f6d1b49c7770aab6944
Iiro Anttila <iiro.a.anttila@gmail.com>
2024-09-12 14:54:20 +0300

    Created handler for listing questionnaires

---
+++ src/functions/questionnaire/list-quiz/handler.ts
@@ -0,0 +1,28 @@
+import QuestionnaireService from "src/apis/quiz-api-service";
+import { middyfy } from "src/libs/lambda";
+import { DocumentClient } from "aws-sdk/clients/dynamodb";
+
+const dynamoDb = new DocumentClient();
+const questionnaireService = new QuestionnaireService(dynamoDb);
+
+/**
+ * Handler for listing all questionnaire entries from DynamoDB.
+ * 
+ * @returns Response object with status code and body.
+ */
+export const listQuestionnairesHandler: APIGatewayProxyHandler = async () => {
+  try {
+    const questionnaireList = await questionnaireService.listQuestionnaires();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(questionnaireList),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to retrieve questionnaire list.', details: error.message }),
+    };
+  }
+};
+
+export const main = middyfy(listQuestionnairesHandler);
