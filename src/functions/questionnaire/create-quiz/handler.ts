d91204c563016ea062950cddea68130d0f10f72a
dalju005-xamk <dalju005@edu.xamk.fi>
2024-09-19 14:04:47 +0300

    Changed createQuizLambda schema typing

--- src/functions/questionnaire/create-quiz/handler.ts
+++ src/functions/questionnaire/create-quiz/handler.ts
@@ -4,6 +4,7 @@ import { DocumentClient } from "aws-sdk/clients/dynamodb";
 import type { QuestionnaireModel } from "src/database/schemas/questionnaire/questionnaire";
 import type { ValidatedEventAPIGatewayProxyEvent } from "src/libs/api-gateway";
 import { questionnaireService } from "src/database/services";
+import { v4 as uuid } from "uuid";
 
 /**
  * Handler for creating a new questionnaire entry in DynamoDB.
@@ -11,16 +12,20 @@ import { questionnaireService } from "src/database/services";
  * @param event - API Gateway event containing the request body.
  * @returns Response object with status code and body.
  */
-export const createQuizHandler: ValidatedEventAPIGatewayProxyEvent<QuestionnaireModel> = async (event) => {
+export const createQuizHandler: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   const { body } = event;
-  const {title,
+  const {
+    title,
     description,
     options,
     tags,
     passedUsers,
     passScore
   } = body;
-  
+
+  const newQuestionnaireId = uuid();
+  let questionnaireResponse: QuestionnaireModel | undefined = undefined;
+
   try {
     if (!event.body) {
       return {
@@ -29,7 +34,7 @@ export const createQuizHandler: ValidatedEventAPIGatewayProxyEvent<Questionnaire
       };
     }
 
-    if (!data.id || !data.title || !data.description || !data.options || !data.tags || !data.passedUsers || !data.passScore) {
+    if (!title || !description || !options || !tags || !passedUsers || !passScore) {
       return {
         statusCode: 400,
         body: JSON.stringify({ error: "Some required data is missing !" })
@@ -37,30 +42,26 @@ export const createQuizHandler: ValidatedEventAPIGatewayProxyEvent<Questionnaire
     }
 
     const createdQuestionnaire = await questionnaireService.createQuestionnaire({
-      id: newQuestionnaireid,
+      id: newQuestionnaireId,
       title: title,
-      description: newQuestionnaire.description,
-      options: newQuestionnaire.options,
-      tags: newQuestionnaire.tags,
-      passedUsers: newQuestionnaire.passedUsers,
-      passScore: newQuestionnaire
+      description: description,
+      options: options,
+      tags: tags,
+      passedUsers: passedUsers,
+      passScore: passScore
     });
 
-
-
+    questionnaireResponse = createdQuestionnaire;
 
     return {
       statusCode: 201,
-      body: JSON.stringify(createdQuestionnaire)
+      body: JSON.stringify(questionnaireResponse)
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({
-        error: "Failed to create questionnaire entry.",
-        details: error.message
-      })
-    };
+      body: `Failed to create questionnaire entry ${error}`
+    }
   }
 };
 
