6257291caf2a8ec334ce714f9582c17711cabd00
Yusuf Farah <cyufa001@edu.xamk.fi>
2024-10-18 14:09:35 +0300

    made a few changes

--- src/libs/auth-utils.ts
+++ src/libs/auth-utils.ts
@@ -16,20 +16,32 @@ interface DecodedToken {
 export const getAuthDataFromToken = (event: { headers: APIGatewayProxyEvent['headers'] }): DecodedToken | null => {
   try {
     const authHeader = event.headers.Authorization || event.headers.authorization;
+    
     if (!authHeader) {
       console.error("Missing Authorization header.");
       return null;
     }
 
     const token = authHeader.split(' ')[1];
-    const decodedToken = jwt.decode(token) as DecodedToken;
 
-    if (!decodedToken || typeof decodedToken === 'string') {
-      console.error("Invalid token.");
+    if (!token) {
+      console.error("Authorization header does not contain a valid token.");
       return null;
     }
 
-    console.log('Decoded JWT Token:', decodedToken);
+    const decodedToken = jwt.decode(token) as DecodedToken;
+
+    if (!decodedToken || typeof decodedToken === 'string') {
+      console.error("Invalid token format or content.");
+      return null;
+    }
+
+    if (!decodedToken.sub) {
+      console.error("Token is missing 'sub' claim.");
+      return null;
+    }
+
+    console.log('Decoded JWT Token:', JSON.stringify(decodedToken, null, 2));
 
     return {
       sub: decodedToken.sub,
