e32585bf9155327a047d72e73da7298f531a8e49
Hanh <phamhuyanh277@gmail.com>
2024-10-18 11:41:49 +0300

    Creating model for VacationRequest

---
+++ src/database/models/vacationRequest.ts
@@ -0,0 +1,30 @@
+/**
+ * DynamoDB model for vacation request
+ */
+interface VacationRequestModel {
+    id: string;
+    personId: string;
+    draft: boolean;
+    startDate: string;
+    endDate: string;
+    days: number;
+    type: VacationType;
+    message: string;
+    createdBy: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+/**
+ * DynamoDB model for vacation type
+ */
+
+enum VacationType {
+    VACATION = "VACATION",
+    UNPAID_TIME_OFF = "UNPAID_TIME_OFF",
+    SICKNESS = "SICKNESS",
+    PERSONAL_DAYS = "PERSONAL_DAYS",
+    MATERNITY_PATERNITY = "MATERNITY_PATERNITY",
+    CHILD_SICKNESS = "CHILD_SICKNESS"
+}
+  
