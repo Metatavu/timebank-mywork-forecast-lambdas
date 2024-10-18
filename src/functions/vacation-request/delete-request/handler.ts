946c7376f3ed87e9198b4f50dbfe692104efe8e4
Hanh <phamhuyanh277@gmail.com>
2024-10-18 12:49:59 +0300

    Creating delete-vacation-request lambdas

---
+++ src/functions/vacation-request/delete-request/handler.ts
@@ -0,0 +1,53 @@
+import type { APIGatewayProxyEvent, APIGatewayProxyHandler} from "aws-lambda";
+import { vacationRequestService } from "src/database/services";
+import { middyfy } from "src/libs/lambda";
+
+/**
+ * Lambda for deleting a vacation request entry from DynamoDB.
+ * 
+ * @param event event
+ */
+
+const deleteVacationRequestHandler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent) => {
+    /*APIGatewayHandler (aka Handler<APIEvent, APIResult>) will take API event and result
+    Received API Event, Return API Result */
+    const { id } = event.pathParameters || {};
+
+    try {
+        if (!id) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    error: "Missing or invalid path parameter: id",
+                }),
+            };
+        }
+
+        const findVacationRequestById = await vacationRequestService.findVacationRequest(id);
+        if (!findVacationRequestById) {
+            return {
+                statusCode: 404,
+                body: JSON.stringify({
+                    error: "Request ${id} not found.",
+                }),
+            };
+        };
+
+        await vacationRequestService.deleteVacationRequest(id);
+
+        return {
+            statusCode: 204,
+            body: JSON.stringify(""),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                error: "Failed to delete request.",
+                details: error.message,
+            }),
+        };
+    }
+}
+
+export const main = middyfy(deleteVacationRequestHandler);
