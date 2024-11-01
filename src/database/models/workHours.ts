a50d767d62891ea0180e1c843d9bb6ed82785969
Stepan Khlebushkin <stepan.khlebushkin@gmail.com>
2024-11-01 13:49:34 +0200

    Created Model for Work Hours

---
+++ src/database/models/workHours.ts
@@ -0,0 +1,43 @@
+import {DateTime} from "luxon";
+
+/**
+ * Severa model for Work Hours
+ */
+interface WorkHoursModel {
+  severaWorkHoursGuid: string;
+  user: UserSubModel;
+  project: ProjectSubModel;
+  phase: PhaseSubModel;
+  description: string;
+  eventDate: Date;
+  quantity: number;
+  startTime: DateTime;
+  endTime: DateTime;
+}
+
+/**
+ * Severa sub model for user
+ */
+interface UserSubModel {
+  severaUserGuid: string;
+  name: string;
+}
+
+/**
+ * Severa sub model for project
+ */
+interface ProjectSubModel {
+  severaProjectGuid: string;
+  name: string;
+  isClosed: boolean;
+}
+
+/**
+ * Severa sub model for phase
+ */
+interface PhaseSubModel {
+  severaPhaseGuid: string;
+  name: string;
+}
+
+export default WorkHoursModel;
