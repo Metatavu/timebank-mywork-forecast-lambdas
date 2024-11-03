e1b62cdb4b72e17de63e08095077c2c54f7537a5
Daniil Zotov <ddazo002@edu.xamk.fi>
2024-11-03 23:04:07 +0200

    renames endpoints

--- src/functions/memo-management/trello-cards/get-board-members/index.ts
+++ src/functions/memo-management/trello-cards/get-board-members/index.ts
@@ -6,7 +6,7 @@ export default {
     {
       httpApi: {
         method: 'get',
-        path: '/card-members',
+        path: '/board-members',
         authorizer: {
           name: "timebankKeycloakAuthorizer"
         }
