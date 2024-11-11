bc65d197297e960b85c5cf9212adb614ef94483b
dalju005-xamk <dalju005@edu.xamk.fi>
2024-11-11 10:35:35 +0200

    index changed + small readme fix

--- src/functions/questionnaire/create-questionnaire/index.ts
+++ src/functions/questionnaire/create-questionnaire/index.ts
@@ -6,7 +6,7 @@ export default {
     {
       httpApi: {
         method: "post",
-        path: "/questionnaire",
+        path: "/questionnaires",
         authorizer: {
           name: "timebankKeycloakAuthorizer",
         },
