cc3eba81ba7dd0439196356a677f979fc58a9532
Ilmo Salomäki <salomaki.ilmo@gmail.com>
2024-11-28 21:20:48 +0200

    Corrected fetched data and billable percentage

--- src/meta-assistant/generic/message-utils.ts
+++ src/meta-assistant/generic/message-utils.ts
@@ -14,19 +14,20 @@ namespace MessageUtilities {
    * @returns a message based on the worked time and the percentage of billable hours
    */
   export const calculateWorkedTimeAndBillableHours = (user: TotalTime| DailyCombinedData): CalculateWorkedTimeAndBillableHoursResponse => {
-    const { quantity, billableTime, enteredHours } = user;
+    const { quantity, billableTime, enteredHours, expectedHours, totalBillableTime } = user;
 
-    const billableHoursPercentage = enteredHours === 0 ? "0" : (billableTime/enteredHours * 100).toFixed(0);
+    const billableHoursPercentage = enteredHours === 0 ? "0" : (totalBillableTime/enteredHours * 100).toFixed(0);
+    const totalOverTime = enteredHours - expectedHours;
 
-    const undertime = TimeUtilities.timeConversion(quantity * -1);
-    const overtime = TimeUtilities.timeConversion(quantity);
+    const undertime = TimeUtilities.timeConversion(totalOverTime * -1);
+    const overtime = TimeUtilities.timeConversion(totalOverTime);
 
     let message = "You worked the expected amount of time";
-    if (quantity > 0) {
+    if (totalOverTime > 0) {
       message = `Overtime: ${overtime}`;
     }
 
-    if (quantity < 0) {
+    if (totalOverTime < 0) {
       message = `Undertime: ${undertime}`;
     }
 
