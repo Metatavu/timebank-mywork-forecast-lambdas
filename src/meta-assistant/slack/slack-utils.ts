a027325c0005851e958c2b44b07b031523ab1426
Ilmo Salomäki <salomaki.ilmo@gmail.com>
2024-12-13 11:25:02 +0200

    Add parameter descriptions for findSlackUser function in Slack utilities

--- src/meta-assistant/slack/slack-utils.ts
+++ src/meta-assistant/slack/slack-utils.ts
@@ -52,6 +52,8 @@ namespace SlackUtilities {
 
   /**
   * Find the corresponding Slack user
+  * @param firstName user first name
+  * @param lastName user last name
   */
   export const findSlackUser = async (firstName: string, lastName: string) => {
     try {
